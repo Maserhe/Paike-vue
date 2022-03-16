d01de2b877a042c9e5a7870cc5bae39a59d9915e
Maserhe <982289931@qq.com>
2022-03-16 19:50:13 +0800

    add管理员界面

---
+++ src/views/manage/Student.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card class="account-container">
+    <h1>学生管理</h1>
+    
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'StudentManage',
+  setup() {
+
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/manage/Teacher.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <el-card class="account-container">
+  <h1>teacher</h1>
+    
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
