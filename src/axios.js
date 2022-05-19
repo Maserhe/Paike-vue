033f0ceaf92e054036e994b9f0ef7d2341b03a76
Maserhe <982289931@qq.com>
2022-05-19 22:00:56 +0800

    更新pdf生成方式

--- src/axios.js
+++ src/axios.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 import router from './router'
 import store from './store'
 
-// axios.defaults.baseURL = "http://81.68.195.233:8899/"
+// axios.defaults.baseURL = "http://121.36.132.192:8899"
 // axios.defaults.baseURL = "http://192.168.77.203:8080/"
 // axios.defaults.baseURL = "http://localhost:8080/"
-// axios.defaults.baseURL = "/"
 // 前置拦截
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem("token")
--- src/utils/htmlToPdf.js
+++ src/utils/htmlToPdf.js
@@ -10,20 +10,32 @@ function getPdf (idStr, title) {
         const contentWidth = canvas.width
         const contentHeight = canvas.height
 
-        var dataUrl = canvas.toDataURL("image/png");
-        var downloadUrl = dataUrl.replace("image/png","image/octet-stream");//图片地址
-        
-        /* 导出不分页处理 */
-        const pageData = canvas.toDataURL('image/jpeg', 1.0)
-        const pdfWidth = (contentWidth + 10) / 2 * 0.75
-        const pdfHeight = (contentHeight + 200) / 2 * 0.75 // 500为底部留白
+        let pageHeight = contentWidth / 592.28 * 841.89
+        let leftHeight = contentHeight
+        // pdf页面向上偏移
+        let position = 0
+        // a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+        let imgWidth = 595.28
+        let imgHeight = 592.28 / contentWidth * contentHeight
+        let pageData = canvas.toDataURL('image/jpeg', 1.0)  //转换图片为dataURL
+        let pdf = new JsPDF('', 'pt', 'a4')
+        // 有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+        // 当内容未超过pdf一页显示的范围，无需分页
+        if (leftHeight < pageHeight) {
+            pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
+        } else {
+            while(leftHeight > 0) {
+            pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+            leftHeight -= pageHeight
+            position -= 841.89
+            //避免添加空白页
+            if(leftHeight > 0) {
+                pdf.addPage()
+            }
+            }
+        }
 
-        const imgWidth = pdfWidth
-        const imgHeight = (contentHeight / 2 * 0.75) // 内容图片这里不需要留白的距离
-
-        const PDF = new JsPDF('', 'pt', [ pdfWidth, pdfHeight, ])
-        PDF.addImage(pageData, 'jpeg', 0, 0, imgWidth, imgHeight)
-        PDF.save(title + '.pdf')
+        pdf.save(title + '.pdf')
     })
 }
 
